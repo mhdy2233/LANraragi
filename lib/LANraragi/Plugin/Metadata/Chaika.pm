7e997881df8897dc5520596b060146a07c4f6f8d
魔皇地狱 <mhdy2233@foxmail.com>
2024-08-08 04:43:19 +0800

    Update Chaika.pm

--- lib/LANraragi/Plugin/Metadata/Chaika.pm
+++ lib/LANraragi/Plugin/Metadata/Chaika.pm
@@ -10,117 +10,116 @@ use LANraragi::Utils::Logging qw(get_plugin_logger);
 
 my $chaika_url = "https://panda.chaika.moe";
 
-#Meta-information about your plugin.
+# 插件的元信息
 sub plugin_info {
 
     return (
-        #Standard metadata
+        # 标准元数据
         name        => "Chaika.moe",
         type        => "metadata",
         namespace   => "trabant",
         author      => "Difegue",
         version     => "2.3.1",
         description =>
-          "Searches chaika.moe for tags matching your archive. This will try to use the thumbnail first, and fallback to a default text search.",
+          "在 chaika.moe 上搜索与你的档案匹配的标签。首先尝试使用缩略图，如果失败，则回退到默认的文本搜索。",
         icon =>
           "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAUCAYAAACNiR0NAAAACXBIWXMAAAsTAAALEwEAmpwYAAAA\nB3RJTUUH4wYCFQocjU4r+QAAAB1pVFh0Q29tbWVudAAAAAAAQ3JlYXRlZCB3aXRoIEdJTVBkLmUH\nAAAEZElEQVQ4y42T3WtTdxzGn/M7J+fk5SRpTk7TxMZkXU84tTbVNrUT3YxO7HA4pdtQZDe7cgx2\ns8vBRvEPsOwFYTDYGJUpbDI2wV04cGXCGFLonIu1L2ptmtrmxeb1JDkvv121ZKVze66f74eH7/f5\nMmjRwMCAwrt4/9KDpflMJpPHvyiR2DPcJklJ3TRDDa0xk36cvrm8vDwHAAwAqKrqjjwXecPG205w\nHBuqa9rk77/d/qJYLD7cCht5deQIIczbgiAEKLVAKXWUiqVV06Tf35q8dYVJJBJem2A7Kwi2nQzD\nZig1CG93+PO5/KN6tf5NKpVqbsBUVVVFUUxwHJc1TXNBoxojS7IbhrnLMMx9pVJlBqFQKBKPxwcB\nkJYgjKIo3QCE1nSKoghbfJuKRqN2RVXexMaQzWaLezyeEUEQDjscjk78PxFFUYRkMsltJgGA3t7e\nyMLCwie6rr8iCILVbDbvMgwzYRjGxe0o4XC4s1AoHPP5fMP5/NNOyzLKAO6Ew+HrDADBbre/Ryk9\nnzx81FXJNlEpVpF+OqtpWu2MpmnXWmH9/f2umZmZi4cOHXnLbILLzOchhz1YerJAs9m1GwRAg2GY\nh7GYah488BJYzYW+2BD61AFBlmX/1nSNRqN9//792ujoaIPVRMjOKHoie3DytVGmp2fXCAEAjuMm\nu7u7Umosho6gjL/u/QHeEgvJZHJ2K/D+/fuL4+PjXyvPd5ldkShy1UXcmb4DnjgQj/fd5gDA6/XS\nYCAwTwh9oT3QzrS1+VDVi+vd3Tsy26yQVoFF3dAXJVmK96p9EJ0iLNOwKKU3CQCk0+lSOpP5WLDz\nF9Q9kZqyO0SloOs6gMfbHSU5NLRiUOuax2/HyZPHEOsLw2SbP83eu/fLxrkNp9P554XxCzVa16MC\n7+BPnTk9cfmH74KJE8nmga7Xy5JkZ8VKifGIHpoBb1VX8hNTd3/t/7lQ3OeXfFPvf/jBRw8ezD/a\n7M/aWq91cGgnJaZ2VcgSdnV1XRNNd3vAoBVVYusmnEQS65hfgSG6c+zy3Kre7nF/KrukcMW0Zg8O\nD08DoJutDxxOEb5IPUymwrq8ft1gLKfkFojkkRxemERCAQUACPFWRazYLJcrFGwQhyufbQQ7rFpy\nLMkCwGZC34qPIuwp+XPOjBFwazQ/txrdFS2GGS/Xuj+pUKLGk1Kjvlded3s72lyGW+PLbGVcmrAA\ngN0wTk1NWYODg9XOKltGtpazi5GigzroUnHN5nUHG1ylRsG7rDXHmnEpu4CeEtEKkqNc6QqlLc/M\n8uT5lLH5eq0aGxsju1O7GQB498a5s/0x9dRALPaQEDZnYwnhWJtMCCNrjeb0UP34Z6e/PW22zjPP\n+vwXBwfPvbw38XnXjk7GsiwKAIQQhjAMMrlsam45d+zLH6/8o6vkWcBcrXbVKQhf6bpucCwLjmUB\nSmmhXC419eblrbD/TAgAkUjE987xE0c7ZDmk66ajUCnq+cL63fErl25s5/8baQPaWLhx6goAAAAA\nSUVORK5CYII=",
         parameters => [
-            { type => "bool", desc => "Add the following tags if available: download URL, gallery ID, category, timestamp" },
+            { type => "bool", desc => "如果可用，添加以下标签：下载 URL、画廊 ID、类别、时间戳" },
             {   type => "bool",
                 desc =>
-                  "Add tags without a namespace to the 'other:' namespace instead, mirroring E-H's behavior of namespacing everything"
+                  "将没有命名空间的标签添加到 'other:' 命名空间中，以镜像 E-H 的所有标签命名空间行为"
             },
             {   type => "string",
-                desc => "Add a custom 'source:' tag to your archive. Example: chaika. Will NOT add a tag if blank"
+                desc => "添加自定义的 'source:' 标签到你的档案中。示例：chaika。如果为空则不添加标签"
             },
             {   type => "bool",
-                desc => "Save the original title when available instead of the English or romanised title"
+                desc => "在可用时保存原始标题，而不是英文或罗马化标题"
             }
         ],
-        oneshot_arg => "Chaika Gallery or Archive URL (Will attach matching tags to your archive)"
+        oneshot_arg => "Chaika 画廊或档案 URL（将把匹配的标签附加到你的档案中）"
     );
 
 }
 
-#Mandatory function to be implemented by your plugin
+# 必须实现的函数，返回一个标签哈希
 sub get_tags {
 
     shift;
-    my $lrr_info = shift;                                       # Global info hash
-    my ( $addextra, $addother, $addsource, $jpntitle ) = @_;    # Plugin parameters
+    my $lrr_info = shift;                                       # 全局信息哈希
+    my ( $addextra, $addother, $addsource, $jpntitle ) = @_;    # 插件参数
 
     my $logger   = get_plugin_logger();
     my $newtags  = "";
     my $newtitle = "";
 
-    # Parse the given link to see if we can extract type and ID
+    # 解析给定的链接以查看是否可以提取类型和 ID
     my $oneshotarg = $lrr_info->{oneshot_param};
     if ( $oneshotarg =~ /https?:\/\/panda\.chaika\.moe\/(gallery|archive)\/([0-9]*)\/?.*/ ) {
         ( $newtags, $newtitle ) = tags_from_chaika_id( $1, $2, $addextra, $addother, $addsource, $jpntitle );
     } else {
 
-        # Try SHA-1 reverse search first
-        $logger->info( "Using thumbnail hash " . $lrr_info->{thumbnail_hash} );
+        # 首先尝试 SHA-1 反向搜索
+        $logger->info( "使用缩略图哈希 " . $lrr_info->{thumbnail_hash} );
         ( $newtags, $newtitle ) = tags_from_sha1( $lrr_info->{thumbnail_hash}, $addextra, $addother, $addsource, $jpntitle );
 
-        # Try text search if it fails
+        # 如果失败，尝试文本搜索
         if ( $newtags eq "" ) {
-            $logger->info("No results, falling back to text search.");
+            $logger->info("没有结果，回退到文本搜索。");
             ( $newtags, $newtitle ) =
               search_for_archive( $lrr_info->{archive_title}, $lrr_info->{existing_tags}, $addextra, $addother, $addsource, $jpntitle );
         }
     }
 
     if ( $newtags eq "" ) {
-        $logger->info("No matching Chaika Archive Found!");
-        return ( error => "No matching Chaika Archive Found!" );
+        $logger->info("未找到匹配的 Chaika 档案！");
+        return ( error => "未找到匹配的 Chaika 档案！" );
     } else {
-        $logger->info("Sending the following tags to LRR: $newtags");
+        $logger->info("将以下标签发送到 LRR: $newtags");
 
-        #Return a hash containing the new metadata
+        # 返回包含新元数据的哈希
         return ( tags => $newtags, title => $newtitle );
     }
 
 }
 
 ######
-## Chaika Specific Methods
+## Chaika 特定方法
 ######
 
 # search_for_archive
-# Uses chaika's html search to find a matching archive ID
+# 使用 Chaika 的 HTML 搜索来找到匹配的档案 ID
 sub search_for_archive {
 
     my $logger = get_plugin_logger();
     my ( $title, $tags, $addextra, $addother, $addsource, $jpntitle ) = @_;
 
-    #Auto-lowercase the title for better results
+    # 自动将标题转为小写以获得更好的结果
     $title = lc($title);
 
-    #Strip away hyphens and apostrophes as they apparently break search
+    # 去除破坏搜索的连字符和撇号
     $title =~ s/-|'/ /g;
 
     my $URL = "$chaika_url/jsearch/?gsp&title=" . uri_escape_utf8($title) . "&tags=";
 
-    #Append language:english tag, if it exists.
-    #Chaika only has english or japanese so I aint gonna bother more than this
+    # 如果标签中包含 language:english，则添加语言标签
     if ( $tags =~ /.*language:\s?english,*.*/gi ) {
         $URL = $URL . uri_escape_utf8("language:english") . "+";
     }
 
-    $logger->debug("Calling $URL");
+    $logger->debug("调用 $URL");
     my $ua  = Mojo::UserAgent->new;
     my $res = $ua->get($URL)->result;
 
     my $textrep = $res->body;
-    $logger->debug("Chaika API returned this JSON: $textrep");
+    $logger->debug("Chaika API 返回的 JSON: $textrep");
 
     my ( $chaitags, $chaititle ) = parse_chaika_json( $res->json->{"galleries"}->[0], $addextra, $addother, $addsource, $jpntitle );
 
     return ( $chaitags, $chaititle );
 }
 
-# Uses the jsearch API to get the best json for a file.
+# 使用 jsearch API 获取最好的 JSON 数据
 sub tags_from_chaika_id {
 
     my ( $type, $ID, $addextra, $addother, $addsource, $jpntitle ) = @_;
@@ -130,21 +129,21 @@ sub tags_from_chaika_id {
 }
 
 # tags_from_sha1
-# Uses chaika's SHA-1 search with the first page hash we have.
+# 使用 Chaika 的 SHA-1 搜索来处理首个页面哈希
 sub tags_from_sha1 {
 
     my ( $sha1, $addextra, $addother, $addsource, $jpntitle ) = @_;
 
     my $logger = get_plugin_logger();
 
-    # The jsearch API immediately returns a JSON.
-    # Said JSON is an array containing multiple archive objects.
-    # We just take the first one.
+    # jsearch API 立即返回 JSON。
+    # JSON 是一个包含多个档案对象的数组。
+    # 我们只取第一个。
     my $json_by_sha1 = get_json_from_chaika( 'sha1', $sha1 );
     return parse_chaika_json( $json_by_sha1->[0], $addextra, $addother, $addsource, $jpntitle );
 }
 
-# Calls chaika's API
+# 调用 Chaika 的 API
 sub get_json_from_chaika {
 
     my ( $type, $value ) = @_;
@@ -158,22 +157,22 @@ sub get_json_from_chaika {
         return;
     }
     my $textrep = $res->body;
-    $logger->debug("Chaika API returned this JSON: $textrep");
+    $logger->debug("Chaika API 返回的 JSON: $textrep");
 
     return $res->json;
 }
 
-# Parses the JSON obtained from the Chaika API to get the tags.
+# 解析从 Chaika API 获得的 JSON 以获取标签
 sub parse_chaika_json {
 
     my ( $json, $addextra, $addother, $addsource, $jpntitle ) = @_;
     my $tags = $json->{"tags"} || ();
     foreach my $tag (@$tags) {
 
-        #Replace underscores with spaces
+        # 将下划线替换为空格
         $tag =~ s/_/ /g;
 
-        #Add 'other' namespace if none
+        # 如果没有命名空间，则添加 'other' 命名空间
         if ( $addother && index( $tag, ":" ) == -1 ) {
             $tag = "other:" . $tag;
         }
@@ -200,7 +199,7 @@ sub parse_chaika_json {
 
     if ( $gallery && $gallery ne "" ) {
 
-        # add custom source, but only if having found gallery
+        # 添加自定义源，但仅在找到画廊时添加
         if ( $addsource && $addsource ne "" ) {
             push( @$tags, "source:" . $addsource );
         }
