a3f8feb6938d0402075f3c329fc9cc2d87ea11de
魔皇地狱 <mhdy2233@foxmail.com>
2024-08-08 04:40:14 +0800

    Update nHentai.pm

--- lib/LANraragi/Plugin/Login/nHentai.pm
+++ lib/LANraragi/Plugin/Login/nHentai.pm
@@ -7,53 +7,49 @@ no warnings 'uninitialized';
 use Mojo::UserAgent;
 use LANraragi::Utils::Logging qw(get_logger);
 
-#Meta-information about your plugin.
+# 插件的元信息
 sub plugin_info {
 
     return (
-		#Standard metadata
-        name      => "nHentai CF Bypass",
+        # 标准元数据
+        name      => "nHentai CF 绕过",
         type      => "login",
         namespace => "nhentaicfbypass",
         author    => "Pheromir",
         version   => "0.1",
         description =>
-          "Bypasses the Cloudflare Javascript-challenge by re-using cookies from your browser. Both CF cookies and the user-agent must originate from the same webbrowser.",
+          "通过重用浏览器中的 Cookie 绕过 Cloudflare 的 JavaScript 挑战。CF Cookie 和用户代理必须来自同一个浏览器。",
         parameters => [
-              { type => "string", desc => "Browser UserAgent string (Can be found at http://useragentstring.com/ for your browser)" },
-			{ type => "string", desc => "csrftoken cookie for domain nhentai.net" },
-			{ type => "string", desc => "cf_clearance cookie for domain nhentai.net" }
+            { type => "string", desc => "浏览器 UserAgent 字符串（可以在 http://useragentstring.com/ 上找到你的浏览器的 UserAgent）" },
+            { type => "string", desc => "用于域 nhentai.net 的 csrftoken cookie" },
+            { type => "string", desc => "用于域 nhentai.net 的 cf_clearance cookie" }
         ]
     );
 
 }
 
-
-# Mandatory function to be implemented by your login plugin
-# Returns a Mojo::UserAgent object only!
+# 必须实现的函数，返回一个 Mojo::UserAgent 对象
 sub do_login {
 
-    # Login plugins only receive the parameters entered by the user.
+    # 登录插件只接收用户输入的参数。
     shift;
     my ( $useragent, $csrftoken, $cf_clearance ) = @_;
     return get_user_agent( $useragent, $csrftoken, $cf_clearance );
 }
 
-# get_user_agent(useragent, cf cookies)
-# Try crafting a Mojo::UserAgent object that can access nHentai.
-# Returns the UA object created.
+# 返回创建的 UserAgent 对象
 sub get_user_agent {
 
     my ( $useragent, $csrftoken, $cf_clearance ) = @_;
 
-    my $logger = get_logger( "nHentai Cloudflare Bypass", "plugins" );
+    my $logger = get_logger( "nHentai Cloudflare 绕过", "plugins" );
     my $ua     = Mojo::UserAgent->new;
 
     if ( $useragent ne "" && $csrftoken ne "" && $cf_clearance ne "") {
-        $logger->info("Useragent and Cookies provided ($useragent $csrftoken $cf_clearance)!");
+        $logger->info("提供了 UserAgent 和 Cookies ($useragent $csrftoken $cf_clearance)!");
         $ua->transactor->name($useragent);
 
-        #Setup the needed cookies
+        # 设置需要的 Cookies
         $ua->cookie_jar->add(
             Mojo::Cookie::Response->new(
                 name   => 'csrftoken',
@@ -73,7 +69,7 @@ sub get_user_agent {
         );
 
     } else {
-        $logger->info("No cookies provided, returning blank UserAgent.");
+        $logger->info("未提供 Cookies，返回空的 UserAgent。");
     }
 
     return $ua;
